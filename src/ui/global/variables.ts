e6ad8161f71a0cc4347b22af658f7391be210ba4
Maksim Safin <crockolaco@yandex.ru>
2020-09-20 16:43:23 +0500

    style(css-variables): add variables

---
+++ src/ui/global/variables.ts
@@ -0,0 +1,18 @@
+import { css } from "styled-components";
+
+export const variables = css`
+    --color-font-primary: #0d0d0d;
+    --color-font-secondary: #999;
+    --color-font-tertiary: #fff;
+
+    --color-error-primary: #cf2c00;
+    --color-error-secondary: #e48b73;
+    --color-error-background: #faeae5;
+
+    --color-success-primary: #30b800;
+
+    --color-button-primary: #45a6ff;
+    --color-button-secondary: #cf2c00;
+
+    --color-background: #fff;
+`;
