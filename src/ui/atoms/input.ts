bdc255779f13290a73ca37af6d2fae8f9929ff09
Maksim Safin <crockolaco@yandex.ru>
2020-09-29 17:10:29 +0500

    style(ui/atoms/input): add styles

--- src/ui/atoms/input.ts
+++ src/ui/atoms/input.ts
@@ -1,3 +1,13 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Input = styled.input``;
+const reset = css`
+    outline: none;
+`;
+
+export const Input = styled.input`
+    ${reset};
+
+    padding: 0.625rem;
+    border: 1px solid var(--color-border);
+    border-radius: 5px;
+`;
