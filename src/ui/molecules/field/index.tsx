6343d143ff7f0ef9c123ae858b6ef14dcdf6f917
Maksim Safin <crockolaco@yandex.ru>
2020-09-29 17:13:22 +0500

    style(ui/molecules/field): add styles

--- src/ui/molecules/field/index.tsx
+++ src/ui/molecules/field/index.tsx
@@ -1,22 +1,40 @@
-import React from "react";
+import * as React from "react";
 import styled from "styled-components";
 
 import { Input, Label } from "ui/atoms";
-
-const FieldStyled = styled.div``;
 
 type TField = {
     name: string;
     label: string;
     type: string;
+    optional?: boolean;
 };
 
+const FieldStyled = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+
+    ${Label} {
+        margin-bottom: 0.3125rem;
+    }
+`;
+
+const Optional = styled.span`
+    position: absolute;
+    right: 0;
+    font-size: 0.75rem;
+    line-height: 1.25rem;
+    color: var(--color-font-secondary);
+`;
+
 export const Field = (props: TField) => {
-    const { name, label, type } = props;
+    const { name, label, type, optional } = props;
 
     return (
         <FieldStyled>
             <Label htmlFor={name}>{label}</Label>
+            {optional && <Optional>Опционально</Optional>}
             <Input id={name} name={name} type={type} />
         </FieldStyled>
     );
