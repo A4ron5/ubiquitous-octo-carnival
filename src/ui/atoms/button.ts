aa7b50b45a47f47a86fdd5da930dcd6d2e16a538
Maksim Safin <crockolaco@yandex.ru>
2020-09-29 17:09:57 +0500

    feat(ui/atoms/button): add styles

--- src/ui/atoms/button.ts
+++ src/ui/atoms/button.ts
@@ -1,3 +1,28 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Button = styled.button``;
+const reset = css`
+    padding: 0;
+    outline: none;
+    border: none;
+`;
+
+export const Button = styled.button`
+    ${reset};
+
+    width: 6.875rem;
+    height: 2.5rem;
+    background: linear-gradient(
+        180deg,
+        var(--color-button-gradient-1-primary) 0%,
+        var(--color-button-gradinet-2-primary) 100%
+    );
+    border-radius: 5px;
+    color: #fff;
+    text-align: center;
+    line-height: 2.5rem;
+    cursor: pointer;
+
+    &:hover {
+        opacity: 0.9;
+    }
+`;
