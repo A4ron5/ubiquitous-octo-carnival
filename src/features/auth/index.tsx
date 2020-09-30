ecc2a59ea56b677671519e7f7b88894d1430546d
Maksim Safin <crockolaco@yandex.ru>
2020-09-30 18:38:35 +0500

    fix(feature/auth): fix

--- src/features/auth/index.tsx
+++ src/features/auth/index.tsx
@@ -5,21 +5,43 @@ import { Button } from "ui/atoms";
 import { Form, Link, Title } from "./ui/index";
 import { Error } from "features/auth/error";
 
+import { submitted, handleChanged } from "features/auth/model";
+
 export const AuthForm = () => {
     const error = { id: "error/auth/failed", explain: "wrong_credentials" };
 
     return (
         <>
             <Logotype />
-            <Form>
+            <Form onSubmit={submitted}>
                 <Title>API-консолька</Title>
                 <Error error={error} />
-                <Field name="login" label="Логин" type="text" />
-                <Field name="sublogin" label="Сублогин" type="text" optional />
-                <Field name="password" label="Пароль" type="password" />
+                <Field
+                    name="login"
+                    label="Логин"
+                    type="text"
+                    handler={handleChanged}
+                />
+                <Field
+                    name="sublogin"
+                    label="Сублогин"
+                    type="text"
+                    handler={handleChanged}
+                    optional
+                />
+                <Field
+                    name="password"
+                    label="Пароль"
+                    type="password"
+                    handler={handleChanged}
+                />
                 <Button>Войти</Button>
             </Form>
-            <Link href="https://github.com/a4ron5" target="_blank" rel="noopener noreferrer">
+            <Link
+                href="https://github.com/a4ron5"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 @A4ron5
             </Link>
         </>
