f9d1c914c82c7d4eb8bac8c329df33808f8c876c
Maksim Safin <crockolaco@yandex.ru>
2020-09-30 18:39:05 +0500

    style(ui/templates/login): change margin

--- src/ui/templates/login/index.tsx
+++ src/ui/templates/login/index.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const Centring = styled.main`
     & > div {
-        margin: 10rem auto 2.5rem;
+        margin: 4rem auto 2.5rem;
     }
 
     & > form {
