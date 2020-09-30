4e8f352e3434a3a7453b82eef98adb690c5bdbab
Maksim Safin <crockolaco@yandex.ru>
2020-09-30 18:34:45 +0500

    chore(webpack config): add env definition

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,48 +2,54 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const dotenv = require("dotenv");
 
-module.exports = (env) => ({
-    entry: "./src/index.tsx",
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        alias: {
-            features: path.resolve(__dirname, "./src/features/"),
-            api: path.resolve(__dirname, "./src/api/"),
-            lib: path.resolve(__dirname, "./src/lib/"),
-            pages: path.resolve(__dirname, "./src/pages/"),
-            ui: path.resolve(__dirname, "./src/ui/")
-        }
-    },
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "build.js"
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "babel-loader",
-                exclude: /(dist|node_modules)/
+module.exports = () => {
+    const envies = dotenv.config().parsed;
+
+    const enviesKeys = Object.keys(envies).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(envies[next]);
+        return prev;
+    }, {});
+
+    return {
+        entry: "./src/index.tsx",
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+            alias: {
+                features: path.resolve(__dirname, "./src/features/"),
+                api: path.resolve(__dirname, "./src/api/"),
+                lib: path.resolve(__dirname, "./src/lib/"),
+                pages: path.resolve(__dirname, "./src/pages/"),
+                ui: path.resolve(__dirname, "./src/ui/")
             }
+        },
+        output: {
+            path: path.join(__dirname, "/dist"),
+            filename: "build.js"
+        },
+        devServer: {
+            historyApiFallback: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "babel-loader",
+                    exclude: /(dist|node_modules)/
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./public/index.html"
+            }),
+            new webpack.DefinePlugin(enviesKeys),
+            new ForkTsCheckerWebpackPlugin({
+                eslint: {
+                    files: "./src/**/*.{ts,tsx,js,jsx}"
+                }
+            })
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html"
-        }),
-        new webpack.DefinePlugin({
-            "process.env.PRODUCTION": env.production || !env.development,
-            "process.env.NAME": JSON.stringify(require("./package.json").name),
-            "process.env.VERSION": JSON.stringify(require("./package.json").version)
-        }),
-        new ForkTsCheckerWebpackPlugin({
-            eslint: {
-                files: "./src/**/*.{ts,tsx,js,jsx}"
-            }
-        })
-    ]
-});
+    }
+};
