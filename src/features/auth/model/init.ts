a63b252ddfc09a08c44bb10bbd54cc56c409ec9e
Maksim Safin <crockolaco@yandex.ru>
2020-10-03 18:30:12 +0500

    fix(features/auth/model): fix and add types

--- src/features/auth/model/init.ts
+++ src/features/auth/model/init.ts
@@ -1,27 +1,18 @@
 import { sample } from "effector";
 
 import { login } from "api/login";
-import {
-    submitted,
-    setField,
-    loginFx,
-    attachedLoginFx,
-    $form,
-    $user
-} from "./index";
+import { submitted, setField, loginFx, $form, $user } from "./index";
 
 loginFx.use(login);
 
-$user
-    .on(loginFx.doneData, (_, payload) => payload)
-    .on(loginFx.pending, (state) => ({ ...state, error: { status: false } }));
+$user.on(loginFx.doneData, (_, payload) => payload);
 
 $form.on(setField, (state, { key, value }) => ({
     ...state,
     [key]: value
 }));
 
-sample({ source: $form, clock: submitted, target: attachedLoginFx });
+sample({ source: $form, clock: submitted, target: loginFx });
 
 submitted.watch((e) => {
     e.preventDefault();
