7ffb4136aef6e45b4d8ff57b4178bf667fb53767
Maksim Safin <crockolaco@yandex.ru>
2020-10-10 16:18:22 +0500

    feat(appliacation): add Redirect

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
+import { Redirect } from "react-router-dom";
 
 import { Pages } from "pages/index";
 import { GlobalStyles } from "ui/global";
 
 const Application = () => (
     <>
+        <Redirect to="/login" />
         <GlobalStyles />
         <Pages />
     </>
