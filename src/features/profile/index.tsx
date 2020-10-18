78481c1fc8c4b815690a4333b627cb3825d39419
Maksim Safin <crockolaco@yandex.ru>
2020-10-18 18:44:21 +0500

    feat(features/profile): add

---
+++ src/features/profile/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import { Info } from "features/profile/ui";
+
+export const Profile = () => {
+    return (
+        <Info>
+            some@email.com <span>:</span> sublogin
+        </Info>
+    );
+};
---
+++ src/features/profile/ui/index.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Info = styled.div`
+    height: 1.875rem;
+    padding: 0.3125rem 0.625rem;
+    border-radius: 5px;
+    border: 1px solid var(--color-border);
+    color: var(--color-font-primary);
+
+    span {
+        color: var(--color-border);
+    }
+`;
