1b8aea75b8c104d8b79617cfe20bf8ce4d9a9914
Maksim Safin <crockolaco@yandex.ru>
2020-10-18 18:39:27 +0500

    feat(ui/templates/home): add

---
+++ src/ui/templates/home/home.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    max-width: 1280px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    background: #f6f6f6;
+`;
+
+export const HomeTemplate: React.FC = ({ children }) => (
+    <Container>{children}</Container>
+);
