bf9724c5d5aa8220ed7c1937be527125c2b26537
Maksim Safin <crockolaco@yandex.ru>
2020-10-18 18:43:31 +0500

    feat(features/github): add

---
+++ src/features/github/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import { Link } from "./ui/index";
+
+export const GithubLink = () => (
+    <Link
+        href="https://github.com/a4ron5"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        @A4ron5
+    </Link>
+);
---
+++ src/features/github/ui/index.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Link = styled.a`
+    display: block;
+    width: fit-content;
+    color: var(--color-font-secondary);
+    text-decoration: none;
+
+    &:hover {
+        opacity: 0.9;
+    }
+`;
