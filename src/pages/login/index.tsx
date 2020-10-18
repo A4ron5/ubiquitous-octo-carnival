8e059882e6530690a3298725f8c4b80e7ca75169
Maksim Safin <crockolaco@yandex.ru>
2020-10-18 18:41:47 +0500

    fix(pages/login): add github link

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 
-import { AuthForm } from "features/auth";
+import { AuthForm } from "features/auth-form";
+import { GithubLink } from "features/github";
+
 import { LoginTemplate } from "ui/templates/login";
 
 export const LoginPage = () => {
     return (
         <LoginTemplate>
             <AuthForm />
+            <GithubLink />
         </LoginTemplate>
     );
 };
