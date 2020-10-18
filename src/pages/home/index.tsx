6db9497bfe082b218725b397032e695d5af83c89
Maksim Safin <crockolaco@yandex.ru>
2020-10-18 18:41:18 +0500

    feat(pages/home): add

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,34 @@
 import * as React from "react";
 
-export const HomePage = () => <div>HomePage</div>;
+import { Profile } from "features/profile";
+import { Logout } from "features/log-out";
+import { History } from "features/history";
+import { GithubLink } from "features/github";
+import { Button } from "features/button";
+
+import { HomeTemplate } from "ui/templates/home/home";
+
+import { Header, Main, Footer } from "pages/home/ui";
+
+import { Logotype } from "ui/molecules";
+import { Title } from "ui/atoms";
+
+export const HomePage = () => {
+    return (
+        <HomeTemplate>
+            <Header>
+                <Logotype />
+                <Title>API-консолька</Title>
+                <Profile />
+                <Logout />
+            </Header>
+            <History />
+            <Main />
+            <Footer>
+                <Button pending={false}>Отправить</Button>
+                <GithubLink />
+                <span>Форматировать</span>
+            </Footer>
+        </HomeTemplate>
+    );
+};
---
+++ src/pages/home/ui/index.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    padding: 0.625rem 1rem;
+    height: 50px;
+
+    h1 {
+        margin-left: 1.25rem;
+        margin-right: auto;
+    }
+
+    h1 + div {
+        margin-right: 1.25rem;
+    }
+`;
+
+export const Main = styled.main``;
+
+export const Footer = styled.footer`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border-top: 1px solid var(--color-border);
+    height: 70px;
+`;
