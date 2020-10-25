3bc94d209b4565e0503b973c4e7840d819a062b0
Maksim Safin <crockolaco@yandex.ru>
2020-10-25 16:45:33 +0500

    fix(ui/global): add height 100vh to body

--- src/ui/global/index.ts
+++ src/ui/global/index.ts
@@ -18,8 +18,13 @@ export const GlobalStyles = createGlobalStyle`
   
   body {
     font-size: 16px;
+    height: 100vh;
     font-family: "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
     color: var(--color-font-primary);
     background: var(--color-app-background);
+  }
+  
+  #root {
+    height: 100%;
   }
 `;
