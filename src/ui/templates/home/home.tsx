aaba5af6e0a771ef04f19d0ec73ad7220e57bcf0
Maksim Safin <crockolaco@yandex.ru>
2020-10-25 16:45:00 +0500

    fix(ui/templates/home): add height 100%

--- src/ui/templates/home/home.tsx
+++ src/ui/templates/home/home.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
     max-width: 1280px;
+    height: 100%;
     margin: 0 auto;
     display: flex;
     flex-direction: column;
