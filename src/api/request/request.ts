5fde52dfbabaf06f6ef9c1481eef7173259b3041
Maksim Safin <crockolaco@yandex.ru>
2020-10-26 18:13:23 +0500

    fix(api): change handle error

--- src/api/request/request.ts
+++ src/api/request/request.ts
@@ -1,11 +1,7 @@
 import { sendsay } from "lib/sendsay";
 
-export const request = async (data) => {
-    try {
-        const res = await sendsay.request(data);
+export const request = async (data: unknown) => {
+    const res = await sendsay.request(data);
 
-        return res;
-    } catch (error) {
-        return error;
-    }
+    return res;
 };
